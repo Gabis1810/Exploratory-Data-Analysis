00102a455cd464372c375e306900d1cbb7806c3d
Gabis1810 <74035563+Gabis1810@users.noreply.github.com>
2021-01-08 16:28:04 -0600

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,46 @@
+# Carlos Gabriel Guerra Farfán
+# Exploratory Data Analysis
+# Week 1
+# Course Project 1
+
+# Load and clean the Table
+t <- read.table("C:/Users/gabis/OneDrive/Documentos/Scribble/7mo Semestre/Actividad Curricular Complementaria/Coursera/Exploratory Data Analysis/Semana 1/household_power_consumption.txt",
+                header = TRUE, sep = ";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
+# Format date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+
+# Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Remove incomplete observation
+t <- t[complete.cases(t),]
+
+# Combine Date and Time column
+dateTime <- paste(t$Date, t$Time)
+
+# Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Remove Date and Time column
+t <- t[ ,!(names(t) %in% c("Date", "Time"))]
+
+# Add DateTime column
+t <- cbind(dateTime, t)
+
+# Format dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+
+# Create Plot 3
+with(t, {
+  plot(Sub_metering_1 ~ dateTime, type = "l",
+       ylab = "Global Active Power (Kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ dateTime, col = 'Red')
+  lines(Sub_metering_3 ~ dateTime, col = 'Blue')
+})
+legend("topright", col = c("black", "red", "blue"), lwd = c(1, 1, 1), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Save file and close device
+dev.copy(png, "plot3.png", width = 480, height = 480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,53 @@
+# Carlos Gabriel Guerra Farfán
+# Exploratory Data Analysis
+# Week 1
+# Course Project 1
+
+# Load and clean the Table
+t <- read.table("C:/Users/gabis/OneDrive/Documentos/Scribble/7mo Semestre/Actividad Curricular Complementaria/Coursera/Exploratory Data Analysis/Semana 1/household_power_consumption.txt",
+                header = TRUE, sep = ";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
+# Format date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+
+# Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Remove incomplete observation
+t <- t[complete.cases(t),]
+
+# Combine Date and Time column
+dateTime <- paste(t$Date, t$Time)
+
+# Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Remove Date and Time column
+t <- t[ ,!(names(t) %in% c("Date", "Time"))]
+
+# Add DateTime column
+t <- cbind(dateTime, t)
+
+# Format dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+
+# Create Plot 4
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+with(t, {
+  plot(Global_active_power ~ dateTime, type = "l", 
+       ylab = "Global Active Power (Kilowatts)", xlab = "")
+  plot(Voltage ~ dateTime, type = "l", 
+       ylab = "Voltage (Volt)", xlab = "")
+  plot(Sub_metering_1 ~ dateTime, type = "l", 
+       ylab = "Global Active Power (Kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ dateTime, col = 'Red')
+  lines(Sub_metering_3 ~ dateTime, col = 'Blue')
+  legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ dateTime, type = "l", 
+       ylab="Global Rective Power (Kilowatts)", xlab = "")
+})
+
+# Save file and close device
+dev.copy(png, "plot4.png", width = 480, height = 480)
+dev.off()
